a7312b6f98b253fd57cb999f3e757d663aad3c31
ramyac6 <ramchalla6@gmail.com>
2018-08-10 21:33:27 -0700

    added hug

---
+++ commands/hug.js
@@ -0,0 +1,9 @@
+//Code borrowed from arynbot <3
+exports.run = (bot, msg, args, level) => {
+	if (!msg.mentions.users.array()[0]) {
+		msg.channel.send(`*hugs* ***${msg.author.username}***`)
+	} else {
+		let huggee = msg.mentions.users.array()[0];
+		msg.channel.send(`*hugs* ***${huggee.username}***`);
+	};
+};
