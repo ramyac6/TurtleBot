f219880f5049babc68f64f4bb812bec9e9a6c08a
ramyac6 <ramchalla6@gmail.com>
2018-08-12 15:56:59 -0700

    added remote messaging

---
+++ commands/repeatspam.js
@@ -0,0 +1,13 @@
+const config = require("../config.json");
+
+exports.run = (bot, msg, args, level) => {
+	if (msg.member.id == config.myUserID || msg.member.id == config.alID) {
+        let mess = msg.content;
+        bot.channels.get(config.spamID).send(mess.substring(mess.indexOf(" ")));
+        bot.channels.get(config.loggingID).send(`${msg.author.username}: ${mess.substring(mess.indexOf(" "))}`);
+        msg.delete();
+    } else {
+        msg.channel.send("hecc you, you're not allowed");
+        msg.delete();
+    }
+};
