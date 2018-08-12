2568df21b7724c2e555254f29f9fa44ae373a793
ramyac6 <ramchalla6@gmail.com>
2018-08-12 15:13:30 -0700

    added repeat

---
+++ commands/repeat.js
@@ -0,0 +1,11 @@
+const config = require("../config.json");
+
+exports.run = (bot, msg, args, level) => {
+	if (msg.member.id == config.alID || msg.member.id == config.myUserID) {
+        let mess = msg.content;
+        msg.channel.send(mess.substring(mess.indexOf(" ")));
+        msg.delete();
+    } else {
+        msg.channel.send("hecc you, you're not allowed");
+    }
+};
