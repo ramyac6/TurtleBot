ac2566a79acbe127a90f241ddc318e47b5f0a8a8
ramyac6 <ramchalla6@gmail.com>
2018-08-12 16:31:24 -0700

    fixed logging messages

--- commands/repeatspam.js
+++ commands/repeatspam.js
@@ -4,7 +4,7 @@ exports.run = (bot, msg, args, level) => {
 	if (msg.member.id == config.myUserID || msg.member.id == config.alID) {
         let mess = msg.content;
         bot.channels.get(config.spamID).send(mess.substring(mess.indexOf(" ")));
-        bot.channels.get(config.loggingID).send(`${msg.author.username}: ${mess.substring(mess.indexOf(" "))}`);
+        bot.channels.get(config.loggingID).send(`${msg.author.username} in #spam-bam: ${mess.substring(mess.indexOf(" "))}`);
         msg.delete();
     } else {
         msg.channel.send("hecc you, you're not allowed");
