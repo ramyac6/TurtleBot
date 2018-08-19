64d747ad180bd5db5bf07f2471e6ed5be2c9f9a3
Ramya C <ramchalla6@gmail.com>
2018-08-18 19:37:24 -0700

    channel name is now in logging message

--- commands/repeat.js
+++ commands/repeat.js
@@ -5,7 +5,7 @@ exports.run = (bot, msg, args, level) => {
 	if (msg.member.id!=110933278193647616) {
         let mess = msg.content;
         msg.channel.send(mess.substring(mess.indexOf(" ")));
-        bot.channels.get(config.loggingID).send(`${msg.author.username} sent to channel: ${mess.substring(mess.indexOf(" "))}`);
+        bot.channels.get(config.loggingID).send(`${msg.author.username} sent to #${msg.channel.name}: ${mess.substring(mess.indexOf(" "))}`);
         msg.delete();
     } else {
         msg.channel.send("hecc you, you're not allowed");
